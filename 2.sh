9d5a9cc650436e24c811c29f0808cf396eba37c1
fbenderick <fbenderick@protonmail.ch>
2018-06-24 00:55:49 +0300

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,59 @@
+echo 'local' > /etc/hostname
+mkinitcpio -p linux
+
+echo 'Setup root password':
+passwd
+
+# install neovim, can't live with vi :\
+pacman -Suy neovim
+
+# create locales
+nvim /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+# setting up console font
+echo FONT=latarcyrheb-sun32 > /etc/vconsole.conf
+
+# set up time
+rm /etc/localtime
+ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc --utc
+
+# BOOTLOADER
+# grub -- for gpt
+# pacman -Suy grub
+# grub-install --target=i386-pc /dev/sda
+# grub-mkconfig -o /boot/grub/grub.cfg
+
+# Intel microcode for UEFI
+pacman -Suy dosfstools intel-ucode
+bootctl --path=/boot install
+# create boot entry with default content
+mkdir -p /boot/loader
+mkdir -p /boot/loader/entries
+rm /boot/loader/entries/arch.conf
+echo 'title Arch Linux' > /boot/loader/entries/arch.conf
+echo 'linux /vmlinuz-linux' >> /boot/loader/entries/arch.conf
+echo 'initrd /intel-ucode.img' >> /boot/loader/entries/arch.conf
+echo 'initrd /initramfs-linux.img' >> /boot/loader/entries/arch.conf
+echo 'options root=PARTUUID= rw quiet' >> /boot/loader/entries/arch.conf
+blkid | awk '{print $1" "$(NF)}' >> /boot/loader/entries/arch.conf
+nvim /boot/loader/entries/arch.conf
+echo "default arch" > /boot/loader/loader.conf
+read -p "It seems loader succesfully installed..."
+
+# create user
+useradd --create-home user
+passwd user
+
+# add user to sudoers
+pacman -Suy sudo
+EDITOR=nvim visudo
+
+# need git to clone boostrap repo
+pacman -Suy git
+
+echo 'Base installation finished'
+echo 'Now, reboot, login with user, clone repo and and run aa scripts from 2-user'
+read -p "Press enter to continue"
