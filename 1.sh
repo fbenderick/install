da8fe9a7fe23ff4d09bf3526c934ff3bff6069b0
fbenderick <fbenderick@protonmail.ch>
2018-06-23 21:02:37 +0300

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,18 @@
+# format partitions
+mkfs.vfat -F32 /dev/sda1
+mkfs.ext4 /dev/sda3
+mkswap /dev/sda2
+swapon /dev/sda2
+
+# install
+mount /dev/sda3 /mnt
+mkdir -p /mnt/boot
+mount /dev/sda1 /mnt/boot
+pacstrap /mnt base base-devel
+
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+# copy install scripts
+cp /root/install/20-chroot.sh /mnt/root/
+# arch-chroot /mnt /root/20-chroot.sh
+# reboot
